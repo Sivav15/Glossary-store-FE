3051b13bd3970563b49965fd2e5ac96537315518
sivav15 <sivanathanv1999@gmail.com>
2023-03-13 17:02:00 +0530

    n

--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Feature from './feature';
 import Categories from './categories';
 import Customer from './customer';
 import Contact from './contact';
 import Footer from './footer';
-
+import { useDispatch } from 'react-redux';
+import { getProduct } from '../../redux/slice/user/productReducer';
 function Home() {
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(getProduct()) 
+  },[])
   return (
     <>
     <Categories/>
